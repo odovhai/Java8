f928bf61a28ed6393e8339907f9cd4b347eda2b1
egolai <dovgaj@gmail.com>
2015-09-16 17:24:58 +0300

    Presentation + first sources

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,37 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+
+        Arrays.asList("One", "Two", "Three", "Four", "Five").stream()
+                .filter(s -> s.length() > 3)
+                .map(String::length)
+                .forEach(System.out::println);
+
+
+
+
+
+//        Arrays.asList("One", "Two", "Three", "Four", "Five").stream()
+//                .filter(new Predicate<String>() {
+//                    @Override
+//                    public boolean test(String s) {
+//                        return s.length() > 3;
+//                    }
+//                })
+//                .map(new Function<String, Integer>() {
+//                    @Override
+//                    public Integer apply(String s) {
+//                        return s.length();
+//                    }
+//                })
+//                .forEach(new Consumer<Integer>() {
+//                    @Override
+//                    public void accept(Integer i) {
+//                        System.out.println(i);
+//                    }
+//                });
     }
 }
